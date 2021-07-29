4bcdb986e9592ac9977a5ba8aec530b651d012f2
Lucas Broto <brotolucasc@gmail.com>
2021-07-29 16:23:22 +0100

    CUT 3 - Third push

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -1,5 +1,6 @@
 from tag_manipulator import TagManipulator
 
+
 def test_split_empty_string_result_empty_array():
     # arrange
     stringToSplit = ""
@@ -23,6 +24,21 @@ def test_split_comma_empty_string_result_empty_array():
 
     # act
     result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_string_result_array_of_one():
+    # arrange
+    stringToSplit = "java"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
 
     # assert
     assert result == expResult
